604b63dc52e84b3612da86562197b1827c736686
Ritvik Avancha <47481413+average-avancha@users.noreply.github.com>
2023-10-02 23:10:56 -0500

    Update nondex_validation.py
    fix main test function with debug statements

--- nondex_validation.py
+++ nondex_validation.py
@@ -14,26 +14,18 @@ def main():
 
     setup_nondex_testing_filestructure(PROJ_ROOT)
     project_flaky_tests = load_flaky_tests(PROJ_ROOT, project_url)
-    
-    # for test in project_flaky_tests:  
-    test = project_flaky_tests[0]
-    log_path = f"{nondex_testing_path}/flaky_testing_logs/{test.module_path}"
-    test.set_nondex_maven_plugin("nondex-maven-plugin:2.1.7-SNAPSHOT")
-    test.run_flaky_test(nondex_testing_path, log_path)
-        
+
     # Check current Java version is 17.0.6 at least
     # ensure_java_17()
 
     # Check Maven version is 3.9.2 at least
     # ensure_maven_3_9_2()
     
-    # Clone compatible repo
-    # repo_https_link = f"{test.project_url}.git"
-    # clone_project_repo(repo_https_link, repo_name, nondex_testing_path)
-
-    # Build repo successfully
-    
-    
+    for test in project_flaky_tests:  
+        log_path = f"{nondex_testing_path}/flaky_testing_logs/{test.module_path}"
+        test.set_nondex_maven_plugin("nondex-maven-plugin:2.1.7-SNAPSHOT")
+        test.run_flaky_test(nondex_testing_path, log_path)
+        
     return
 
 
